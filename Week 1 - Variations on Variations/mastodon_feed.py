9686ef661629d403729cfe4057fadd2fca6cda1e
Jonas Betzendahl <jonas.betzendahl@fau.de>
2020-01-12 13:22:02 +0100

    rework to work with traces, factor out mastodon

---
+++ Week 1 - Variations on Variations/mastodon_feed.py
@@ -0,0 +1,17 @@
+from mastodon import Mastodon
+
+import variations
+
+# Set up Mastodon
+mastodon = Mastodon(
+    access_token = 'token.secret',
+    api_base_url = 'https://botsin.space'
+)
+
+# Generate with default parameters:
+(filename,trace) = variations.variation()
+
+# Post to Mastodon
+media  = mastodon.media_post("imgs/" + filename)
+status = "Variation No. " + str(counter) + "\nTrace: " + trace
+mastodon.status_post(status, media_ids=media)
--- Week 1 - Variations on Variations/variations.py
+++ Week 1 - Variations on Variations/variations.py
@@ -2,138 +2,178 @@ import math
 import os
 import random
 
-from mastodon import Mastodon
-from PIL      import Image, ImageDraw
+from PIL import Image, ImageDraw
 
 # Some Geometric Functions we'll need later.
+
+# Standard euclidean distance between two points on the plane.
 def distance(p1,p2):
-    return math.sqrt((p1[0]-p2[0])**2 + (p1[1]-p2[1])**2)
+	return math.sqrt((p1[0]-p2[0])**2 + (p1[1]-p2[1])**2)
 
-# Circle Intersection we'll need later.
+# Intersection points of two circles.
 def get_intersections(x0, y0, r0, x1, y1, r1):
-    d = distance((x1,y1), (x0,y0))
+	d = distance((x1,y1), (x0,y0))
 
-    # non intersecting
-    if d > r0 + r1 :
-        return None
-    # One circle within other
-    if d < abs(r0-r1):
-        return None
-    # coincident circles
-    if d == 0 and r0 == r1:
-        return None
-    else:
-        a=(r0**2-r1**2+d**2)/(2*d)
-        h=math.sqrt(r0**2-a**2)
-        x2=x0+a*(x1-x0)/d
-        y2=y0+a*(y1-y0)/d
-        x3=x2+h*(y1-y0)/d
-        y3=y2-h*(x1-x0)/d
+	# non intersecting
+	if d > r0 + r1 :
+		return None
+	# One circle within other
+	if d < abs(r0-r1):
+		return None
+	# coincident circles
+	if d == 0 and r0 == r1:
+		return None
+	else:
+		a=(r0**2-r1**2+d**2)/(2*d)
+		h=math.sqrt(r0**2-a**2)
+		x2=x0+a*(x1-x0)/d
+		y2=y0+a*(y1-y0)/d
+		x3=x2+h*(y1-y0)/d
+		y3=y2-h*(x1-x0)/d
 
-        x4=x2-h*(y1-y0)/d
-        y4=y2+h*(x1-x0)/d
+		x4=x2-h*(y1-y0)/d
+		y4=y2+h*(x1-x0)/d
 
-        return [(x3, y3), (x4, y4)]
+		return [(x3, y3), (x4, y4)]
 
 def find_center(p1, p2, ps, radius):
-    i1 = get_intersections(p1[0], p1[1], radius, p2[0], p2[1], radius)
+	i1 = get_intersections(p1[0], p1[1], radius, p2[0], p2[1], radius)
 
-    assert(i1 is not None)
-    assert(len(i1) == 2)
+	assert(i1 is not None)
+	assert(len(i1) == 2)
 
-    xs = list(map((lambda tp:tp[0]), ps))
-    ys = list(map((lambda tp:tp[1]), ps))
+	xs = list(map((lambda tp:tp[0]), ps))
+	ys = list(map((lambda tp:tp[1]), ps))
 
-    x_avg = sum(xs) / len(xs)
-    y_avg = sum(ys) / len(ys)
+	x_avg = sum(xs) / len(xs)
+	y_avg = sum(ys) / len(ys)
 
-    d1 = distance(i1[0], (x_avg,y_avg))
-    d2 = distance(i1[1], (x_avg,y_avg))
+	d1 = distance(i1[0], (x_avg,y_avg))
+	d2 = distance(i1[1], (x_avg,y_avg))
 
-    if d1 < d2:
-        center = i1[0]
-    else:
-        center = i1[1]
+	if d1 < d2:
+		center = i1[0]
+	else:
+		center = i1[1]
 
-    return center
+	return center
 
-# Set up Mastodon
-mastodon = Mastodon(
-    access_token = 'token.secret',
-    api_base_url = 'https://botsin.space'
-)
+def variation_by_trace(trace):
+	elems = trace.split("-")
 
-# Calculate Polygon Coordinates
+def variation(px = None, max_poly = None, deg = None, 
+                             inds = None, cols = None):
 
-n = 3 + random.randint(2,6)
-gons = []
+	# Setup and bookkeeping
+	n = 3 + random.randint(2,6) if max_poly is None else max_poly
+	
+	# String to identify this particular image by.
+	trace = str(n) + "-"
 
-for m in range(n,2,-1):
+	gons = []
+	sz   = 1000 if px is None else px
 
-    gon = []
+	if inds is not None:
+		assert(len(inds) == n-3)
 
-    if m == n:
-        radius = 475
-        theta  = math.radians(random.randint(0,360))
-        
-        for k in range(0,m):
-            x = 500 + (radius * math.cos(2 * math.pi * k/m + theta))
-            y = 500 + (radius * math.sin(2 * math.pi * k/m + theta)) 
-            gon.append((round(x),round(y)))
-    else:
-        oldgon = gons[-1]
-        index  = random.randint(0,len(oldgon)-1)
-        p1 = oldgon[index-1] # Works even when index is 0. Thank you, python!
-        p2 = oldgon[index]
+	if cols is not None:
+		assert(len(cols) == n-2)
 
-        length = distance(p1,p2)
-        radius = length / (2 * math.sin(math.pi / m))
+	# counting variable so I don't have to think up formulas.
+	j = 0
 
-        center = find_center(p1,p2,oldgon,radius)
+	# Calculate Polygon Coordinates
+	for m in range(n,2,-1):
+		gon = []
+		if m == n:
+			radius = 0.95 * (sz / 2)
+			winkel = random.randint(0,359) if deg is None else deg
+			trace  = trace + str(winkel) + "-"
+			theta  = math.radians(winkel) # Angles almost always in radians.
 
-        champ = float('inf')
-        theta = 0
-        for w in range(0,3600):
-            a = center[0] + (radius * math.cos(math.radians(w/10.0)))
-            b = center[1] + (radius * math.sin(math.radians(w/10.0)))
-            if distance((a,b), p1) < champ:
-                theta = math.radians(w/10.0)
-                champ = distance((a,b), p1)
+			for k in range(0,m):
+				x = (sz / 2) + (radius * math.cos(2 * math.pi * k/m + theta))
+				y = (sz / 2) + (radius * math.sin(2 * math.pi * k/m + theta))
+				gon.append((round(x),round(y)))
 
-        for k in range(0,m):
-            x = center[0] + (radius * math.cos(2 * math.pi * k/m + theta))
-            y = center[1] + (radius * math.sin(2 * math.pi * k/m + theta)) 
-            gon.append((round(x),round(y)))
+			gons.append(gon)
 
-    gons.append(gon)
+		else:
 
-# Create Image
+			old   = gons[-1]
+			index = random.randint(0,len(old)-1) if inds is None else inds[j]
+			trace = trace + str(index) + "-"
+			j += 1
 
-colours = ["gray", "black", "red", "maroon", "olive", "green", "teal", "blue", "navy", "fuchsia", "purple", "deeppink", "darkorange", "orangered", "gold", "indigo", "springgreen", "lightseagreen"]
+			p1 = old[index-1] # Works even when index is 0. Thank you, python!
+			p2 = old[index]
 
-image = Image.new(mode = "RGB", size = (1000,1000), color = (255,255,255))
-draw  = ImageDraw.Draw(image)
+			# Calculating radius of polygon from one of its edges.
+			length = distance(p1,p2)
+			radius = length / (2 * math.sin(math.pi / m))
+			
+			center = find_center(p1,p2,old,radius)
 
-used = []
-for gon in gons:
-    c = random.choice(colours)
-    while c in used:
-        c = random.choice(colours)
-    used.append(c)
-    draw.polygon(gon, fill = c)
+			champ  = float('inf')
+			theta  = 0
+			factor = 1000.0	# This is big because time spent on each image is
+											# not very important, but neat fit of polygons is!
 
-# Save Image
+			for w in range(0,360 * int(factor)):
+				a = center[0] + (radius * math.cos(math.radians(w/factor)))
+				b = center[1] + (radius * math.sin(math.radians(w/factor)))
 
-if not os.path.exists('imgs'):
-    os.makedirs('imgs')
+				if distance((a,b), p1) < champ:
+					theta = math.radians(w/factor)
+					champ = distance((a,b), p1)
 
-counter  = len(os.listdir('imgs'))
-filename = str(counter) + ".png"
+			for k in range(0,m):
+				x = center[0] + (radius * math.cos(2 * math.pi * k/m + theta))
+				y = center[1] + (radius * math.sin(2 * math.pi * k/m + theta))
+				gon.append((round(x),round(y)))
 
-image.save('imgs/' + filename)
+			gons.append(gon)
 
-# Post to Mastodon
+	# Create Image
 
-media  = mastodon.media_post("imgs/" + filename)
-status = "Variation No. " + str(counter)
-mastodon.status_post(status, media_ids=media)
+	colours = ["gray", "black", "red", "maroon", "olive", "green", "teal",
+							"blue", "navy", "fuchsia", "purple", "deeppink",
+							"darkorange", "orangered", "gold", "indigo",
+							"springgreen", "lightseagreen"]
+
+	white = (255,255,255)
+	image = Image.new(mode = "RGB", size = (sz,sz), color = white)
+	draw  = ImageDraw.Draw(image)
+
+	# Assign colours to polygons
+	used   = []
+	colour = ""
+	for gon in gons:
+		if cols is not None:
+			colour = colours[j]
+			used.append(colour)
+			j += 1
+			trace = trace + str(j) + "-"
+		else:
+			c_index = random.randint(0,len(colours)-1)
+			colour  = colours[c_index] 
+			while colour in used:
+				c_index = random.randint(0,len(colours)-1)
+				colour  = colours[c_index]
+			trace = trace + str(c_index) + "-"  
+			used.append(colour)
+		
+		draw.polygon(gon, fill = colour)
+
+	# Save Image
+	if not os.path.exists('imgs'):
+		os.makedirs('imgs')
+
+	counter  = len(os.listdir('imgs'))
+	filename = str(counter) + ".png"
+	image.save('imgs/' + filename)
+
+	trace = trace + str(sz)
+
+	print("Generated " + filename + " successfully! Trace:", trace)
+	return (filename, trace)
